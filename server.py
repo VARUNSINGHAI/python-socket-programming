6f54f9e2be91d5aa2513d292c48f042a64a107e7
RV_01 <40372745+VARUNSINGHAI@users.noreply.github.com>
2019-09-26 03:08:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 import socket
 import sys
 def create_socket():
+  try:
+    global host
+    global port
+    global s
+    host=""
+    port=9999
+    s=socket.error as msg:
+      print("socket creation error"+str(msg))
