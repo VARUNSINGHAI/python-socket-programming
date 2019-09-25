d44edd6f0bd9ddcffb61f3d0af5965f5cefe7022
RV_01 <40372745+VARUNSINGHAI@users.noreply.github.com>
2019-09-26 03:21:07 +0530

    Update server.py
    binding+listening the socket

--- server.py
+++ server.py
@@ -7,5 +7,21 @@ def create_socket():
     global s
     host=""
     port=9999
-    s=socket.error as msg:
-      print("socket creation error"+str(msg))
+    s=socket.socket()
+  except socket.error as msg:
+    print("socket creation error"+str(msg))
+#binding the socket and listening to the connection
+def bind_socket():
+  try:
+    global host
+    global port
+    global s
+    print("binding the port"+str(port))
+    s.bind((host,port))#this format is called cuple in python
+    s.listen(5)
+  except socket.error as msg:
+    print("socket creation error"+str(msg)+"\n"+"retrying...")
+    bind_socket() #recursive call if the connection was made
+    
+ 
+    
